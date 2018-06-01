e498e1157fa941b2d6194af6d1be3b1776806b23
nataliacosta <nataliadiasdacosta@hotmail.com>
2018-05-31 22:53:02 -0300

    grid on header

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="container-big"> 
+    <div class="container-big">
     <nav>
         <a href="{{ $page->baseUrl }}/bsi"><div class="{{ $page->icone_ativo('bsi') }} grande bsi"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iBSI.png') }}" alt="bsi"><br>BACHARELADO&nbsp;&nbsp;EM<br><span class="subtitleGrande">SISTEMAS&nbsp;DE<br>INFORMA&Ccedil;&Atilde;O</span></div></a>
         <div style="float:left;">
@@ -11,8 +11,44 @@
             <a href="{{ $page->baseUrl }}/oportunidades"><div class= "{{ $page->icone_ativo('oportunidades') }} normal oportunidades"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iOportunidades.png') }}" alt="oportunidades"><br>OPORTUNIDADES</p></div></a>
             <a href="{{ $page->baseUrl }}/secretaria"><div class= "{{ $page->icone_ativo('secretaria') }} normal secretaria"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iSecretaria.png') }}" alt="secretaria"><br>SECRETARIA</p></div></a>
             <a href="{{ $page->baseUrl }}/institucional"><div class= "{{ $page->icone_ativo('institucional') }} normal institucional"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iInstitucional.png') }}" alt="institucional"><br>INSTITUCIONAL<br>EIA-CCET-UNIRIO</p></div></a> 
-        </div>  
+        </div>
         <a href="{{ $page->baseUrl }}/noticias"><div class="noticias {{ $page->icone_ativo('noticias') }} grande semMargem"><p class="subtitle subtitleGrande"><img src="{{ $page->adiciona_imagem('iNoticias.png') }}" alt="notícias"><br>NOT&Iacute;CIAS</p></div></a>
-    </nav> 
-    </div> 
+    </nav>
+    </div>
+</header>
+<br>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4" style="background-color: red;">1</div>
+            <div class="col-lg-4" style="background-color: blue;">2</div>
+            <div class="col-lg-4" style="background-color: green;">3</div>
+        </div>
+    </div>
+
+<header>
+    <div class="container-big">
+        <nav class="d-flex align-items-center justify-content-center">
+            <div class="container">
+                <div class="row">
+                    <a href="{{ $page->baseUrl }}/bsi" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class="{{ $page->icone_ativo('bsi') }} bsi w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iBSI.png') }}" alt="bsi"><br>BACHARELADO&nbsp;&nbsp;EM<br><span class="subtitleGrande">SISTEMAS&nbsp;DE<br>INFORMA&Ccedil;&Atilde;O</span></div></a>
+                    <div class="col-6 p-0">
+                        <div class="container">
+                            <div class="row h-50">
+                                <a href="{{ $page->baseUrl }}/disciplinas" class="col-3 pt-0 pl-0 pbr-10"><div class="{{ $page->icone_ativo('disciplinas') }} disciplinas w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iDisciplinas.png') }}" alt="disciplinas"><br>DISCIPLINAS</p></div></a>
+                                <a href="{{ $page->baseUrl }}/alunos" class="col-6 pt-0 pl-0 pbr-10"><div class= "{{ $page->icone_ativo('alunos') }} alunos w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iAlunos.png') }}" alt="alunos"><br>ALUNOS</p></div></a>
+                                <a href="{{ $page->baseUrl }}/tcc" class="col-3 pt-0 pl-0 pbr-10"><div class= "{{ $page->icone_ativo('tcc') }} tcc w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iTCC.png') }}" alt="Trabalho de Conclusão de Curso"><br>TCC</p></div></a>
+                            </div>
+                            <div class="row h-50">
+                                <a href="{{ $page->baseUrl }}/professores" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('professores') }} professores w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iProfessores.png') }}" alt="Professores"><br>PROFESSORES</p></div></a>
+                                <a href="{{ $page->baseUrl }}/oportunidades" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('oportunidades') }} oportunidades w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iOportunidades.png') }}" alt="oportunidades"><br>OPORTUNIDADES</p></div></a>
+                                <a href="{{ $page->baseUrl }}/secretaria" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('secretaria') }} secretaria w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iSecretaria.png') }}" alt="secretaria"><br>SECRETARIA</p></div></a>
+                                <a href="{{ $page->baseUrl }}/institucional" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('institucional') }} institucional w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iInstitucional.png') }}" alt="institucional"><br>INSTITUCIONAL<br>EIA-CCET-UNIRIO</p></div></a> 
+                            </div>
+                        </div>
+                    </div>
+                    <a href="{{ $page->baseUrl }}/noticias" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class="{{ $page->icone_ativo('noticias') }} noticias w-100 h-100 semMargem"><p class="subtitle subtitleGrande"><img src="{{ $page->adiciona_imagem('iNoticias.png') }}" alt="notícias"><br>NOT&Iacute;CIAS</p></div></a>
+                </div>
+            </div>
+        </nav>
+    </div>
 </header>
