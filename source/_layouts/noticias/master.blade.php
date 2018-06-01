21723513991392bd2c31ab44623aa6799c59d3de
nataliacosta <nataliadiasdacosta@hotmail.com>
2018-06-01 11:37:16 -0300

    changes on padding (content master)

--- source/_layouts/noticias/master.blade.php
+++ source/_layouts/noticias/master.blade.php
@@ -7,7 +7,7 @@
 			<div class="d-none d-lg-block col-lg-3 pt-0 pl-0 pbr-10">
 				@include('_partials.aside.noticias')
 			</div>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 p-r-5 p-xs-l-5 p-sm-l-5">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-9 p-r-5 p-xs-l-5 p-sm-l-3 p-md-l-1">
 				@yield('content')
 			</div>
 		</div>
