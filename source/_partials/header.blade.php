676a434a53ae0e8cdb4b1a67e63ab58ba63a21c2
nataliacosta <nataliadiasdacosta@hotmail.com>
2018-06-01 00:47:40 -0300

    adjust header row

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -6,12 +6,12 @@
                     <a href="{{ $page->baseUrl }}/bsi" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class="{{ $page->icone_ativo('bsi') }} bsi w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iBSI.png') }}" alt="bsi"><br>BACHARELADO&nbsp;&nbsp;EM<br><span class="subtitleGrande">SISTEMAS&nbsp;DE<br>INFORMA&Ccedil;&Atilde;O</span></div></a>
                     <div class="col-6 p-0">
                         <div class="container">
-                            <div class="row h-50">
+                            <div class="row linha-maior">
                                 <a href="{{ $page->baseUrl }}/disciplinas" class="col-3 pt-0 pl-0 pbr-10"><div class="{{ $page->icone_ativo('disciplinas') }} disciplinas w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iDisciplinas.png') }}" alt="disciplinas"><br>DISCIPLINAS</p></div></a>
                                 <a href="{{ $page->baseUrl }}/alunos" class="col-6 pt-0 pl-0 pbr-10"><div class= "{{ $page->icone_ativo('alunos') }} alunos w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iAlunos.png') }}" alt="alunos"><br>ALUNOS</p></div></a>
                                 <a href="{{ $page->baseUrl }}/tcc" class="col-3 pt-0 pl-0 pbr-10"><div class= "{{ $page->icone_ativo('tcc') }} tcc w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iTCC.png') }}" alt="Trabalho de Conclusão de Curso"><br>TCC</p></div></a>
                             </div>
-                            <div class="row h-50">
+                            <div class="row">
                                 <a href="{{ $page->baseUrl }}/professores" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('professores') }} professores w-100 h-100"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iProfessores.png') }}" alt="Professores"><br>PROFESSORES</p></div></a>
                                 <a href="{{ $page->baseUrl }}/oportunidades" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('oportunidades') }} oportunidades w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iOportunidades.png') }}" alt="oportunidades"><br>OPORTUNIDADES</p></div></a>
                                 <a href="{{ $page->baseUrl }}/secretaria" class="col-3 pt-0 pl-0 pbr-10 pb-0"><div class= "{{ $page->icone_ativo('secretaria') }} secretaria w-100 h-100"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iSecretaria.png') }}" alt="secretaria"><br>SECRETARIA</p></div></a>
