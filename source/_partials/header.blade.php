b0600bd6fafee675ea0c26d6479e838c0257562c
nataliacosta <nataliadiasdacosta@hotmail.com>
2018-05-31 22:56:11 -0300

    deleted header trash tests

--- source/_partials/header.blade.php
+++ source/_partials/header.blade.php
@@ -1,32 +1,5 @@
 <header>
     <div class="container-big">
-    <nav>
-        <a href="{{ $page->baseUrl }}/bsi"><div class="{{ $page->icone_ativo('bsi') }} grande bsi"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iBSI.png') }}" alt="bsi"><br>BACHARELADO&nbsp;&nbsp;EM<br><span class="subtitleGrande">SISTEMAS&nbsp;DE<br>INFORMA&Ccedil;&Atilde;O</span></div></a>
-        <div style="float:left;">
-            <a href="{{ $page->baseUrl }}/disciplinas"><div class="{{ $page->icone_ativo('disciplinas') }} normal disciplinas"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iDisciplinas.png') }}" alt="disciplinas"><br>DISCIPLINAS</p></div></a>
-            <a href="{{ $page->baseUrl }}/alunos"><div class= "{{ $page->icone_ativo('alunos') }} largo alunos"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iAlunos.png') }}" alt="alunos"><br>ALUNOS</p></div></a>
-            <a href="{{ $page->baseUrl }}/tcc"><div class= "{{ $page->icone_ativo('tcc') }} normal tcc"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iTCC.png') }}" alt="Trabalho de Conclusão de Curso"><br>TCC</p></div></a>
-            <br style="clear: both;">
-            <a href="{{ $page->baseUrl }}/professores"><div class= "{{ $page->icone_ativo('professores') }} normal professores"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iProfessores.png') }}" alt="Professores"><br>PROFESSORES</p></div></a>
-            <a href="{{ $page->baseUrl }}/oportunidades"><div class= "{{ $page->icone_ativo('oportunidades') }} normal oportunidades"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iOportunidades.png') }}" alt="oportunidades"><br>OPORTUNIDADES</p></div></a>
-            <a href="{{ $page->baseUrl }}/secretaria"><div class= "{{ $page->icone_ativo('secretaria') }} normal secretaria"><p class="subtitle"><img class="icon" src="{{ $page->adiciona_imagem('iSecretaria.png') }}" alt="secretaria"><br>SECRETARIA</p></div></a>
-            <a href="{{ $page->baseUrl }}/institucional"><div class= "{{ $page->icone_ativo('institucional') }} normal institucional"><p class="subtitle"><img src="{{ $page->adiciona_imagem('iInstitucional.png') }}" alt="institucional"><br>INSTITUCIONAL<br>EIA-CCET-UNIRIO</p></div></a> 
-        </div>
-        <a href="{{ $page->baseUrl }}/noticias"><div class="noticias {{ $page->icone_ativo('noticias') }} grande semMargem"><p class="subtitle subtitleGrande"><img src="{{ $page->adiciona_imagem('iNoticias.png') }}" alt="notícias"><br>NOT&Iacute;CIAS</p></div></a>
-    </nav>
-    </div>
-</header>
-<br>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-4" style="background-color: red;">1</div>
-            <div class="col-lg-4" style="background-color: blue;">2</div>
-            <div class="col-lg-4" style="background-color: green;">3</div>
-        </div>
-    </div>
-
-<header>
-    <div class="container-big">
         <nav class="d-flex align-items-center justify-content-center">
             <div class="container">
                 <div class="row">
