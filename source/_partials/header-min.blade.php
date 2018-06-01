2a74d14ab6fa625dec6b2c26add00ec917bbe934
nataliacosta <nataliadiasdacosta@hotmail.com>
2018-06-01 16:47:32 -0300

    added material icons

--- source/_partials/header-min.blade.php
+++ source/_partials/header-min.blade.php
@@ -1,10 +1,9 @@
 <header class="header-min">
     <div class="container-small h-100">
-        <!--<div class="d-flex align-items-center justify-content-center h-100">
-            BUTTON
-        </div>-->
-        <div class="d-flex align-items-center justify-content-center h-100">
+        <div class="d-flex align-items-center justify-content-between h-100 ml-3 mr-3">
+            <i class="mdi mdi-3x mdi-menu text-white"></i>
             <p class="subtitle-small"><img src="{{ $page->adiciona_imagem('iBSI.png') }}" class="bsi-pequeno" alt="bsi"><br>BACHARELADO&nbsp;&nbsp;EM<br><span class="subtitle-smallGrande">SISTEMAS&nbsp;DE<br>INFORMA&Ccedil;&Atilde;O</span>
+            <div></div>
         </div>
     </div>
 </header>
